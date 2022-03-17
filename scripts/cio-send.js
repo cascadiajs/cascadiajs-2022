e1e8fc7a47360a0c1bb8411429531355a9ece067
Carter Rabasa <carter.rabasa@gmail.com>
2022-03-17 15:50:27 -0700

    tmp script, may delete later

---
+++ scripts/cio-send.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch')
+
+async function main() {
+    let payload = {
+        "to": "carter@cascadiajs.com",
+        "transactional_message_id": "2",
+        "message_data": {
+        "customer": {
+            "first_name": "Carter"
+        },
+        "greeting": "Hello, Cascadian!"
+        },
+        "identifiers": {
+        "email": "carter@cascadiajs.com"
+        }
+    }
+
+    // call customer.io API
+    let response = await fetch(`https://api.customer.io/v1/send/email`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer XXX' 
+        },
+        body:    JSON.stringify(payload),
+    })
+
+    const result = await response.json()
+    console.log(result)
+}
+
+main()
