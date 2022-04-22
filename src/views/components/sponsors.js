b524bb24cf9299967981041127a16899b731e754
Carter Rabasa <carter.rabasa@gmail.com>
2022-04-22 14:24:21 -0700

    added fission

--- src/views/components/sponsors.js
+++ src/views/components/sponsors.js
@@ -3,12 +3,16 @@ let sponsors = require('../../shared/data/sponsors.json')
 module.exports = function SponsorsContainer () {
     return /*html*/`
         <div class="sponsors-grid">
-        ${ ['platinum', 'gold', 'silver', 'bronze'].map(tier => /*html*/`
+        ${ ['platinum', 'gold', 'silver', 'bronze', 'community'].map(tier => /*html*/`
             ${ sponsors.filter(s => s.tier === tier).length > 0 ? /*html*/`
-                <div class="tier-wrap"><div class="tier-inner"><div class="tier">${ tier }</div></div></div>
-                <div class="${ tier }">
+                <div class="tier-wrap"><div class="tier-inner"><div class="tier-label">${ tier }</div></div></div>
+                <div class="${ tier } tier">
                 ${ sponsors.filter(s => s.tier === tier).map(s => /*html*/`
-                    <div class="shake-slow"><a href="/sponsors/${ s.key }"><img src="/images/sponsors/${ s.logo }" alt="${ s.name } logo"/></a></div>`).join('')}
+                    ${ ['platinum', 'gold', 'silver'].indexOf(tier) >= 0
+                        ? /*html*/`<div class="shake-slow"><a href="/sponsors/${ s.key }"><img src="/images/sponsors/${ s.logo }" alt="${ s.name } logo"/></a></div>`  
+                        : /*html*/`<div class="shake-slow"><img src="/images/sponsors/${ s.logo }" alt="${ s.name } logo"/></div>`
+                    }
+                `).join('') }
                 </div>
             `: ''}        
         `).join('')}
