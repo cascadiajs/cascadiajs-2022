4ac5e09c85ed548e2c3c0f978b0d2f1252ab4864
Carter Rabasa <carter.rabasa@gmail.com>
2022-05-04 19:06:17 -0700

    preload bg image

--- src/views/layout/social.js
+++ src/views/layout/social.js
@@ -8,9 +8,10 @@ module.exports = function SocialLayout ({ header, image = '/images/sunriver/cano
     <link rel="stylesheet" href="/styles/normalize.css">
     <link rel="stylesheet" href="/styles/main.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preload" href="/images/social-sharing-card.png" as="image">
   </head>
   <body>
-    <div id="social-share">
+    <div style="background-image: url('/images/social-sharing-card.png'); height: 628px; width: 1200px;">
       <img id="social-image" src="${ image }" />
       <div style="width:268px;font-family:alkaline;font-size:25px;line-height:1.125em;font-weight:400;color:#112378;position:absolute;top:470px;left:205px;transform: rotate(-15deg);">
         ${ caption }
