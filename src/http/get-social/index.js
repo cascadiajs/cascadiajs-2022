c88e4c2c3e130a54774d9ba85b8ed26ef093adea
Carter Rabasa <carter.rabasa@gmail.com>
2022-05-04 17:26:40 -0700

    store images to S3

--- src/http/get-social/index.js
+++ src/http/get-social/index.js
@@ -1,3 +1,5 @@
+let AWS = require('aws-sdk')
+let crypto = require('crypto')
 let arc = require('@architect/functions')
 let screenshot = require('./screenshot')
 
@@ -6,12 +8,27 @@ let screenshot = require('./screenshot')
  */
 async function Social (req) {
   const { path } = req.queryStringParameters
+  const s3 = new AWS.S3()
   try {
     let file = await screenshot({ path })
+    let urlhash = crypto
+    .createHash('sha1')
+    .update(path)
+    .digest('base64')
+    let Key = urlhash + '.png'
+    await s3
+      .putObject({
+        Bucket: process.env.ARC_STATIC_BUCKET,
+        Key,
+        Body: file,
+        ACL: 'public-read',
+      })
+      .promise()
+    console.log(`${ process.env.BEGIN_STATIC_ORIGIN}/${ Key }`)
     return {
       type: 'image/png',
       length: file.length,
-      'Cache-Control': 'max-age=86400',
+      'Cache-Control': 'public, max-age=86400',
       body: file
     }
   }
