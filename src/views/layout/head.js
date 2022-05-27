3ebb7c62503bf576a0552a806bd40f5bd88b4fbe
Carter Rabasa <carter.rabasa@gmail.com>
2022-05-27 09:17:21 -0700

    adding First Party

--- src/views/layout/head.js
+++ src/views/layout/head.js
@@ -52,6 +52,11 @@ module.exports = function Head ({path, title, socialUrl, excerpt = null, scripts
     <meta name="twitter:title" content="${ title }">
     <link id="light-scheme-icon" rel="icon" href="/images/logo-blue.svg">
     <link id="dark-scheme-icon" rel="icon" href="/images/logo-green.svg">
+    <script>
+      !function(){var t=window.firstparty=window.firstparty||[];if(!t.initialize){if(t.invoked)return void(window.console&&console.error&&console.error("Firstparty snippet included twice."));t.invoked=!0,t.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"],t.factory=function(r){return function(){var e=Array.prototype.slice.call(arguments);return e.unshift(r),t.push(e),t}};for(var r=0;r<t.methods.length;r++){var e=t.methods[r];t[e]=t.factory(e)}t.load=function(r,e,i){t._writeKey=r,t._host=e,t._firstpartyOptions=i;var a="/js/firstparty.min.js";void 0!==i&&void 0!==i.libraryPath&&(a=i.libraryPath);var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src="https://"+e+a;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(o,n)},t.SNIPPET_VERSION="0.1.0"}}();
+      firstparty.load("zOVuuTNvEYFHM3sP", "fp.cascadiajs.com");
+      firstparty.page();
+    </script>
     ${ scripts.map(s => script(s)) }
     ${ rawHead }
   </head>
