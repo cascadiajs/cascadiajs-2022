379a02695098e95d5317ac4640ebb25f2885dcf3
Carter Rabasa <carter.rabasa@gmail.com>
2022-06-09 07:35:58 -0700

    fixed JSON

--- src/views/speaker.js
+++ src/views/speaker.js
@@ -40,7 +40,7 @@ let Template = function(speaker) {
                         <h3>Links</h3>
                         <div class="person-links">
                         ${ twitter ? `<div><i class="fab fa-twitter"></i> <a target="_blank" href="https://twitter.com/${ twitter }">@${ twitter }</a></div>` : '' }
-                        ${ url ? `<div><i class="fa fa-globe"></i> <a href="${ url }">${ url.split("://")[1] }</a></div>` : '' }
+                        ${ url ? `<div><i class="fa fa-globe"></i> <a target="_blank" href="${ url }">${ url.split("://")[1] }</a></div>` : '' }
                         </div>
                     </div>
                 </div>
