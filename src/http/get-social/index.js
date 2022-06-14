24fd3836dc0ec71581dba1a160445e1269d5b815
Carter Rabasa <carter.rabasa@gmail.com>
2022-06-14 09:18:29 -0700

    caching social sharing images

--- src/http/get-social/index.js
+++ src/http/get-social/index.js
@@ -1,35 +1,54 @@
-//let crypto = require('crypto')
-//let data = require('@begin/data')
+let crypto = require('crypto')
+let data = require('@begin/data')
+let AWS = require('aws-sdk')
 let arc = require('@architect/functions')
 let screenshot = require('./screenshot')
 
-/**
- *
- */
 async function Social (req) {
-  const { path } = req.queryStringParameters
-  try {
-    let file = await screenshot({ path })
-    /*let table = 'social'
-    let key = crypto
-      .createHash('sha1')
-      .update(path)
-      .digest('base64')
-    /data.set({ table, key, file })
-    console.log(table, key, file.length)*/
-    return {
-      type: 'image/png',
-      length: file.length,
-      'Cache-Control': 'public, max-age=86400',
-      body: file
+  const { path, rebuild } = req.queryStringParameters
+
+  let table = 'social'
+  
+  // use the path to generate a hash value
+  let key = crypto
+  .createHash('sha1')
+  .update(path)
+  .digest('base64')
+
+  // see if this image reference exists in the DB
+  let record = data.get({table, key })
+  
+  // if it does not or we are triggering a rebuild, build and store the image
+  if (!record || rebuild) {
+    try {
+      // build the image
+      let file = await screenshot({ path })
+      // store it in S3
+      const s3 = new AWS.S3()
+      let fileName = `social-${ key }.png`
+      await s3
+        .putObject({
+          Bucket: process.env.ARC_STATIC_BUCKET,
+          Key : process.env.ARC_STATIC_PREFIX + '/' + fileName,
+          ContentType: 'image/png',
+          Body: file,
+          ACL: 'public-read',
+        })
+        .promise()  
+      // store a record in the DB
+      data.set({table, key, path, created: Date.now()})
+    }
+    catch (error) {
+      console.log(error)
+      return {
+        statusCode: 500,
+        body: error.toString()
+      }
     }
   }
-  catch (error) {
-    console.log(error)
-    return {
-      statusCode: 500,
-      body: error.toString()
-    }
+
+  return {
+    location: `${ process.env.BEGIN_STATIC_ORIGIN }/social-${ key }.png`
   }
 }
 
