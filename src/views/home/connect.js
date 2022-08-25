99f6e9eb035b06798e388589eb4c9149ff635b2f
Carter Rabasa <carter.rabasa@gmail.com>
2022-08-25 15:20:23 -0700

    added more height to retool widget iframe

--- src/views/home/connect.js
+++ src/views/home/connect.js
@@ -6,7 +6,7 @@ module.exports = async function({ ticket, connections }) {
             <div class=page-title><div><h1>Connect 🤝</h1></div></div>
             <div class=page-body class=narrow>
             <h2>Make a Connection</h2>
-            <iframe id="retool-app" height="400" scrolling="no" allow="camera" style="border:none" src="https://retoolin.tryretool.com/embedded/public/3997468d-a0cf-4d2f-b18e-055db698b133?auth_hash=${ encodeURIComponent(ticket.auth_hash) }"></iframe>
+            <iframe id="retool-app" height="525" scrolling="no" allow="camera" style="border:none" src="https://retoolin.tryretool.com/embedded/public/3997468d-a0cf-4d2f-b18e-055db698b133?auth_hash=${ encodeURIComponent(ticket.auth_hash) }"></iframe>
             <h2>Your Connections</h2>
             ${ connections.map((c) => /*html*/`
                 <details>
